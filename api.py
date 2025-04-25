33534550eaa7dead32d0e59737e1a46f5cce450b
Jeffrey Wood <jeffreywood.dev@gmail.com>
2025-04-25 11:09:18 -0400

    init

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        return 'POST'
+    else:
+        return "<p>Hello World!</p>"
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
